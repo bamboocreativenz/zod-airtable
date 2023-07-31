2359f437c7e70d665d38f009df7aa51d0c71f803
Daniel Lewis <dan.lewis@bamboocreative.nz>
2023-07-31 15:09:36 +0200

    add jsdoc

--- src/types/queryParams.ts
+++ src/types/queryParams.ts
@@ -17,4 +17,16 @@ export const zSelectQueryParams = z
 	})
 	.partial()
 
+/**
+ * @param fields Array of field names or fieldIds.
+ * @param filterByFormula Airtable formula to filter by. Recommended to test in Airtable first.
+ * @param maxRecords Number of records to return.
+ * @param pageSize Number of records to return per page.
+ * @param sort Field names/Ids to sort by and the direction to sort in.
+ * @param view The table view to filter by.
+ * @param cellFormat The cell format to return
+ * @param timeZone The time zone to use for date fields.
+ * @param userLocale The user locale to use
+ * @param returnFieldsByFieldId Whether to return fields by name or by fieldId.
+ */
 export type SelectQueryParamsT = z.infer<typeof zSelectQueryParams>
