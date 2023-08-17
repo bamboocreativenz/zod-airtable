34627a929f532dcc20f0aada3cb3a90248df8446
Daniel Lewis <dan.lewis@bamboocreative.nz>
2023-08-17 19:52:59 +0200

    update to user new errorType signature

--- src/utils/catchErrors.ts
+++ src/utils/catchErrors.ts
@@ -1,16 +1,20 @@
 import { Err } from "ts-results-es"
+import { ErrorType } from "../../errorTypes"
 
-export default function catchErrors(err: unknown | Error | string) {
+export default function catchErrors(
+	errorType: ErrorType,
+	error: unknown | Error | string
+) {
 	// Dev logging
 	if (process.env.NODE_ENV !== "production") {
-		console.error({ err })
+		console.error({ error })
 	}
 
-	if (typeof err === "string") {
-		return new Err(err)
-	} else if (err instanceof Error) {
-		return new Err(err)
+	if (typeof error === "string") {
+		return new Err({ errorType, error })
+	} else if (error instanceof Error) {
+		return new Err({ errorType, error: error.message })
 	} else {
-		return new Err(new Error(JSON.stringify(err)))
+		return new Err({ errorType, error: JSON.stringify(error) })
 	}
 }
