39e808f46b88ad694f60d00cd58587b6707e39bb
Daniel Lewis <dan.lewis@bamboocreative.nz>
2023-08-17 19:53:12 +0200

    init errorTypes

---
+++ errorTypes.ts
@@ -0,0 +1,5 @@
+export enum ErrorType {
+	NoAuth = "User is Unauthenticated",
+	NoPermission = "You do not have permission to take that action",
+	APIError = "Error occured fetching data from the API",
+}
