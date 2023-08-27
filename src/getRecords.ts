65e0f0104d59f6bb3101b49560aefaed446dd671
Daniel Lewis <dan.lewis@bamboocreative.nz>
2023-08-27 19:48:32 +0200

    refactor getRecordsById into seperate file

--- src/getRecords.ts
+++ src/getRecords.ts
@@ -12,94 +12,43 @@ import { RecordZ } from "./types/record"
 import { SchemaZodT } from "./types/schema"
 import { SelectQueryParamsT } from "./types/queryParams"
 
-export interface GetRecordsWithIds<T extends SchemaZodT> {
-	tableId: string
+export interface GetRecords<T extends SchemaZodT> {
+	airtable: AirtableSDK | string
 	baseId: string
-	schema: T
-	recordIds: string[]
-}
-
-export interface GetRecordsWithQuery<T extends SchemaZodT> {
 	tableId: string
-	baseId: string
 	schema: T
 	query?: SelectQueryParamsT
 }
-
-export interface GetRecordsWithIdsAndAirtable<T extends SchemaZodT>
-	extends GetRecordsWithIds<T> {
-	airtable: AirtableSDK
-}
-
-export interface GetRecordsWithQueryAndAirtable<T extends SchemaZodT>
-	extends GetRecordsWithQuery<T> {
-	airtable: AirtableSDK
-}
-export interface GetRecordsWithIdsAndApiKey<T extends SchemaZodT>
-	extends GetRecordsWithIds<T> {
-	apiKey: string
-}
-
-export interface GetRecordsWithQueryAndApiKey<T extends SchemaZodT>
-	extends GetRecordsWithQuery<T> {
-	apiKey: string
-}
-
-export interface GetRecordsWithAirtable<T extends SchemaZodT>
-	extends GetRecordsWithIdsAndAirtable<T>,
-		GetRecordsWithQueryAndAirtable<T>,
-		GetRecordsWithIdsAndApiKey<T>,
-		GetRecordsWithQueryAndApiKey<T> {}
 
 //TODO Add jsdoc as the types are not clear
 export function getRecords<T extends SchemaZodT>({
 	tableId,
 	baseId,
 	schema,
-	...otherArgs
-}:
-	| GetRecordsWithIdsAndAirtable<T>
-	| GetRecordsWithQueryAndAirtable<T>
-	| GetRecordsWithIdsAndApiKey<T>
-	| GetRecordsWithQueryAndApiKey<T>) {
-	let airtable: AirtableSDK
-	if ("airtable" in otherArgs) {
-		airtable = otherArgs.airtable
+	airtable,
+	query,
+}: GetRecords<T>) {
+	let _airtable: AirtableSDK
+	if (airtable instanceof AirtableSDK) {
+		_airtable = airtable
 	} else {
-		airtable = new AirtableSDK({ apiKey: otherArgs.apiKey })
+		airtable = new AirtableSDK({ apiKey: airtable })
 	}
 
 	// we overwrite the base RecordZ fields with the specific schema provided
 	const recordWithSchema = RecordZ.extend({
 		fields: schema,
 	})
-
-	if ("recordIds" in otherArgs) {
-		//TODO Consider moving to Promise.settled to account for request errors (how does airtable sdk handle this in .all() function?)
-		//DL Noticing: Here is a case where we are relying on the Airtable SDK to manage rate limiting/throttling and retrys
-		return Promise.all(
-			otherArgs.recordIds.map(async (id) => {
-				return airtable.base(baseId).table(tableId).find(id)
-			})
-		).then((records) => {
+	return airtable
+		.base(baseId)
+		.table(tableId)
+		.select(query)
+		.all()
+		.then((records) => {
 			return Promise.all(
 				records.map((r) => {
 					return recordWithSchema.safeParseAsync(r)
 				})
 			)
 		})
-	} else {
-		return airtable
-			.base(baseId)
-			.table(tableId)
-			.select(otherArgs.query)
-			.all()
-			.then((records) => {
-				return Promise.all(
-					records.map((r) => {
-						return recordWithSchema.safeParseAsync(r)
-					})
-				)
-			})
-	}
 }
